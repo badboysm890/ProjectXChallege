b68b378881302e3e9183ed2e4ef4ef2fb28a863e
Praveen G <praveensm890@gmail.com>
2019-03-28 16:15:40 +0530

    Add files via upload

---
+++ Charmander Blocker/background.js
@@ -0,0 +1,12 @@
+//no use for this bakground right now 
+function blockRequest(details) 
+{ 
+    return {cancel: true}; 
+} 
+function updateFilters(urls) 
+{
+     if(chrome.webRequest.onBeforeRequest.hasListener(blockRequest)) 
+     chrome.webRequest.onBeforeRequest.removeListener(blockRequest); 
+     chrome.webRequest.onBeforeRequest.addListener(blockRequest, 
+        {urls: ["*://*.google.com/*"]}, ['blocking']); 
+} updateFilters();
